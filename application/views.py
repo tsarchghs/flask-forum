4ba1044b278d8b341902f97b1249c68f9baa99d7
gjergjk71 <gjergjk71@gmail.com>
2018-08-17 01:33:25 +0200

    Created index route

--- application/views.py
+++ application/views.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from application import app
+from application.category.models import Category
 
-@app.route("/health")
-def check_status():
-    return "Status OK"
+@app.route("/")
+def index():
+    categories = Category.query.all()
+    return render_template("index.html",categories=categories)
