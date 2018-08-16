c08f588908b628c6e65e390411939d4dd805d918
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 02:56:23 +0200

    Finished back-end of login view

--- application/authentication/auth.py
+++ application/authentication/auth.py
@@ -1,7 +1,9 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,redirect,request
 from flask_login import LoginManager, UserMixin, login_required,current_user,login_user,logout_user
-from .models import User
+from .models import User,db
+from .forms import LoginForm
+import bcrypt
 
 auth = Blueprint('auth', __name__,template_folder="templates/auth")
 login_manager = LoginManager()
@@ -10,6 +12,22 @@ login_manager = LoginManager()
 def load_user(ID):
 	return User.query.get(ID)
 
-@auth.route('/login')
+@auth.route('/login',methods=["GET","POST"])
 def login():
-    return render_template('login.html')
+	if current_user.is_authenticated:
+		return redirect("/index")
+	form = LoginForm(request.form)
+	if request.method == "GET":
+		return render_template('login.html',form=form)
+	elif request.method == "POST":
+		if not form.validate():
+			return render_template('login.html',form=form,invalid_form=True)
+		password = form.password.data
+		user = User.query.filter_by(username=form.username.data).first()
+		if user:
+			access = bcrypt.checkpw(password.encode('utf8'),user.password)
+			if access:
+				login_user(user)
+		else:
+			return render_template('login.html',form=form,
+									invalid_credentials=True)			
