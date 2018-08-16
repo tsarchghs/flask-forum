ab8033069661dca8703b6216acf28160b145c6ca
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 02:21:48 +0200

    Created load_user func

--- application/authentication/auth.py
+++ application/authentication/auth.py
@@ -1,8 +1,15 @@
 
 from flask import Blueprint, render_template
+from flask_login import LoginManager, UserMixin, login_required,current_user,login_user,logout_user
+from .models import User
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint('auth', __name__,template_folder="templates/auth")
+login_manager = LoginManager()
 
-@auth.route('/')
-def authmain():
-    return render_template('authmain.html')
+@login_manager.user_loader
+def load_user(ID):
+	return User.query.get(ID)
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')
