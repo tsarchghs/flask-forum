573f40394036b7761d77d3dc0bb8fb38030c272d
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 21:59:41 +0200

    showCategory route now filters by slug

--- application/category/category.py
+++ application/category/category.py
@@ -1,10 +1,13 @@
-from flask import Blueprint, render_template,redirect,request,url_for
+from flask import Blueprint, render_template,redirect,request,url_for,abort
 from .models import Category
+
 category = Blueprint("category",
 					 __name__,
 					 template_folder="templates/category")
 
-@category.route("/<int:ID>")
-def showCategory(ID):
-	category = Category.query.get_or_404(ID)
+@category.route("/<string:slug>")
+def showCategory(slug):
+	category = Category.query.filter_by(slug=slug).first()
+	if not category:
+		abort(404)
 	return render_template("showCategory.html",category=category)
