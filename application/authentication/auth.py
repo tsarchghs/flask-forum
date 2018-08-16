74ef94c4cfe0150a654cf32e1c2a0977a62c4cd2
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 03:13:27 +0200

    Created register view

--- application/authentication/auth.py
+++ application/authentication/auth.py
@@ -2,7 +2,7 @@
 from flask import Blueprint, render_template,redirect,request
 from flask_login import LoginManager, UserMixin, login_required,current_user,login_user,logout_user
 from .models import User,db
-from .forms import LoginForm
+from .forms import LoginForm,RegisterForm
 import bcrypt
 
 auth = Blueprint('auth', __name__,template_folder="templates/auth")
@@ -11,6 +11,26 @@ login_manager = LoginManager()
 @login_manager.user_loader
 def load_user(ID):
 	return User.query.get(ID)
+
+@auth.route("/register",methods=["GET","POST"])
+def register():
+	if current_user.is_authenticated:
+		return redirect("/index")
+	form = RegisterForm(request.form)
+	if request.method == "GET":
+		return render_template("register.html",form=form)
+	elif request.method == "POST":
+		if not form.validate():
+			return render_template('register.html',form=form,invalid_form=True)
+		password = form.password.data
+		hashed_password = bcrypt.hashpw(password.encode("utf8"),bcrypt.gensalt())
+		user = User(username=form.username.data,
+					email=form.email.data,
+					password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		login_user(user)
+		return redirect("/index")
 
 @auth.route('/login',methods=["GET","POST"])
 def login():
--- application/authentication/forms.py
+++ application/authentication/forms.py
@@ -1,7 +1,17 @@
-from wtforms import Form, BooleanField, StringField, PasswordField, validators
+from wtforms import Form, BooleanField, StringField, validators
+from wtforms.fields.html5 import EmailField
 
 class LoginForm(Form):
 	username = StringField('Username',
 						[validators.DataRequired(message="Username field is required!")])
 	password = StringField('Password',
 							[validators.DataRequired(message="Password field is required!")])
+
+class RegisterForm(Form):
+	username = StringField("Username",
+							[validators.DataRequired(message="Username field is required!")])
+	password = StringField("Password",
+							[validators.DataRequired(message="Password field is required!")])
+	email = EmailField("Email",
+						[validators.DataRequired(message="Email field is required!"),
+						validators.Email()])
