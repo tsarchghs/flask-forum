bb2c5376050633cf09c127a29a0382a4051ec395
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 03:13:41 +0200

    Defined app.secret_key

--- application/__init__.py
+++ application/__init__.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{}/db.sqlite3'.format(str(os.getcwd()))
 app.config.from_object(os.environ.get('SETTINGS'))
-
+app.secret_key = os.urandom(24)
 with app.app_context():
 	login_manager.init_app(app)
 	auth_db.init_app(app)
