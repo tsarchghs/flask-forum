20b38a139106556cffa2192a8cd3962bf6269f82
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 20:10:13 +0200

    Created showCategory view

--- application/category/category.py
+++ application/category/category.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, render_template,redirect,request,url_for
-
+from .models import Category
 category = Blueprint("category",
 					 __name__,
 					 template_folder="templates/category")
 
-@category.route("/")
-def showCategory():
-	pass
+@category.route("/<int:ID>")
+def showCategory(ID):
+	category = Category.query.get_or_404(ID)
+	return render_template("showCategory.html",category=category)
