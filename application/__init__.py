2c5e2425c0c64b2b9ba75f2af4f8e0e05a1e6ebb
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 02:56:44 +0200

    Initialized login_manager

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from application.authentication.auth import auth
+from application.authentication.auth import auth,login_manager
 from application.authentication.models import db as auth_db
 import os
 
@@ -8,6 +8,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////{}/db.sqlite3'.format(str(os
 app.config.from_object(os.environ.get('SETTINGS'))
 
 with app.app_context():
+	login_manager.init_app(app)
 	auth_db.init_app(app)
 	auth_db.create_all()
 app.register_blueprint(auth, url_prefix='/auth')
