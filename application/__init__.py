5550a472761532a7416cd1f978a5c60cc727f315
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 02:01:52 +0200

    Registered auth blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from application.authentication.auth import auth
 import os
 
 app = Flask(__name__)
 
 app.config.from_object(os.environ.get('SETTINGS'))
+app.register_blueprint(auth, url_prefix='/auth')
