2809b17f0c124849fdbb0df575cbc21f70a6c694
gjergjk71 <gjergjk71@gmail.com>
2018-08-16 21:58:19 +0200

    Added column slug to Category model

--- application/category/models.py
+++ application/category/models.py
@@ -1,9 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
+from slugify import slugify
 
 db = SQLAlchemy()
 
 class Category(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
 	name = db.Column(db.String)
+	slug = db.Column(db.String)
+
+	def __init__(self,id,name):
+		self.id = id
+		self.name = name
+		self.slug =  slugify(self.name)
+
 	def __repr__(self):
-		return "<Category {self.id}"
+		return f"<Category {self.id}>"
