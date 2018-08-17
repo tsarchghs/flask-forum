94197361e9a5cabd01e72c671207a26101f56bd4
gjergjk71 <gjergjk71@gmail.com>
2018-08-17 16:20:00 +0200

    Added category_id column to Forum model

--- application/forum/models.py
+++ application/forum/models.py
@@ -1,10 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from slugify import slugify
+from application.category.models import Category
 
 db = SQLAlchemy()
 
 class Forum(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
+	category_id = db.Column(db.Integer,db.ForeignKey(Category.id))
 	name = db.Column(db.String,unique=True)
 	slug = db.Column(db.String,unique=True)
 	description = db.Column(db.String)
