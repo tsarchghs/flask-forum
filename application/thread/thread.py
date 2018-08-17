0f21ed40f16455215cf98e3205e14ed473f4d253
gjergjk71 <gjergjk71@gmail.com>
2018-08-18 01:23:18 +0200

    Created deleteThread view

--- application/thread/thread.py
+++ application/thread/thread.py
@@ -57,3 +57,19 @@ def editThread(thread_slug):
 		thread.description = form.description.data
 		thread_db.session.commit()
 		return redirect(url_for("thread.showThread",thread_slug=thread.slug))
+
+@thread.route("/delete/<string:thread_slug>",methods=["GET","POST"])
+@login_required
+def deleteThread(thread_slug):
+	thread = Thread.query.filter_by(slug=thread_slug).first()
+	if not thread:
+		abort(404)
+	elif not thread.user_id == current_user.id:
+		abort(401)
+	forum = Forum.query.get_or_404(thread.forum_id)
+	if request.method == "GET":
+		return render_template("delete_thread_confirmation.html",thread=thread)
+	elif request.method == "POST":
+		thread_db.session.delete(thread)
+		thread_db.session.commit()
+		return redirect(url_for("forum.showForum",forum_slug=forum.slug))
