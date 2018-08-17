3c964d7e7b16ca131a3770312ec8896db6ba8979
gjergjk71 <gjergjk71@gmail.com>
2018-08-18 00:05:28 +0200

    Added forum_id column to Thread model

--- application/thread/models.py
+++ application/thread/models.py
@@ -1,13 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from application.forum.models import Forum
-
+from application.authentication.models import User
 db = SQLAlchemy()
 
 class Thread(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
-	user_id = db.Column(db.Integer,db.ForeignKey(Forum.id))
+	forum_id = db.Column(db.Integer,db.ForeignKey(Forum.id))
+	user_id = db.Column(db.Integer,db.ForeignKey(User.id))
 	title = db.Column(db.String(150))
 	description = db.Column(db.String)
 	def __repr__(self):
 		return f"<Thread {self.id}>"
-		
