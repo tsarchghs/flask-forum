6e27feba44046818d2117b02fc9dae82f9922837
gjergjk71 <gjergjk71@gmail.com>
2018-08-17 16:25:25 +0200

    index passes dict of categury:forums to index.html

--- application/views.py
+++ application/views.py
@@ -1,8 +1,13 @@
 from flask import render_template
 from application import app
 from application.category.models import Category
+from application.forum.models import Forum
+from collections import OrderedDict
 
 @app.route("/")
 def index():
     categories = Category.query.all()
-    return render_template("index.html",categories=categories)
+    category_forums = OrderedDict()
+    for category in categories:
+    	category_forums[category] = Forum.query.filter_by(category_id=category.id).all()
+    return render_template("index.html",category_forums=category_forums)
