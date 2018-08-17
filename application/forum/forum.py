f53ebf41ecd9b2bbfd7b0f30553aa4eab601f5bc
gjergjk71 <gjergjk71@gmail.com>
2018-08-17 21:45:44 +0200

    Created editForum view

--- application/forum/forum.py
+++ application/forum/forum.py
@@ -29,3 +29,25 @@ def createForum(category_slug):
 		forum_db.session.add(forum)
 		forum_db.session.commit()
 		return redirect("/")
+
+@forum.route("/edit/<string:forum_slug>",methods=["GET","POST"])
+@login_required
+def editForum(forum_slug):
+	forum = Forum.query.filter_by(slug=forum_slug).first()
+	if not current_user.account_type == "administrator":
+		abort(401)
+	elif not forum:
+		abort(404)
+	form = ForumForm(request.form)
+	if request.method == "GET":
+		return render_template("editForum.html",form=form,forum=forum)
+	elif request.method == "POST":
+		if not form.validate():
+			return render_template("editForum.html",form=form,forum=forum)
+		elif form.name.data != forum.name:
+			if Forum.query.filter_by(name=form.name.data).all():
+				return render_template("editForum.html",form=form,forum=forum,name_taken=True)
+		forum.name = form.name.data
+		forum.description = form.description.data
+		forum_db.session.commit()
+		return redirect("/")
