40bc4c6680804d7502716c0cb5c09bbf48d6c40c
gjergjk71 <gjergjk71@gmail.com>
2018-08-17 15:58:12 +0200

    Added unique=True to name/slug columns

--- application/category/models.py
+++ application/category/models.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 
 class Category(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
-	name = db.Column(db.String)
-	slug = db.Column(db.String)
+	name = db.Column(db.String,unique=True)
+	slug = db.Column(db.String,unique=True)
 
 	def __init__(self,name):
 		self.name = name
