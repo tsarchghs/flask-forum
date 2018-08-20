2ec9fa89d6f88cdf886bf81136b46ee9a43855b1
gjergjk71 <gjergjk71@gmail.com>
2018-08-20 19:54:37 +0200

    Added user_id column to Post model

--- application/post/models.py
+++ application/post/models.py
@@ -1,11 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 from application.thread.models import Thread
+from application.authentication.models import User
 from datetime import datetime
 
 db = SQLAlchemy()
 
 class Post(db.Model):
 	id = db.Column(db.Integer,primary_key=True)
+	user_id = db.Column(db.Integer,db.ForeignKey(User.id))
 	thread_id = db.Column(db.Integer,db.ForeignKey(Thread.id))
 	content = db.Column(db.String)
 	created_on = db.Column('created_on' , db.DateTime,default=datetime.utcnow())
