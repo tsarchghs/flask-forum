3899565a39759801c06347f128b7ab802223ec75
gjergjk71 <gjergjk71@gmail.com>
2018-08-20 19:26:05 +0200

    Registered post blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -3,6 +3,7 @@ from application.authentication.auth import auth,login_manager
 from application.category.category import category
 from application.forum.forum import forum
 from application.thread.thread import thread
+from application.post.post import post
 from application.authentication.models import db as auth_db
 from application.category.models import db as category_db
 from application.forum.models import db as forum_db
@@ -30,3 +31,4 @@ app.register_blueprint(auth, url_prefix='/auth')
 app.register_blueprint(category,url_prefix="/category")
 app.register_blueprint(forum,url_prefix="/forum")
 app.register_blueprint(thread,url_prefix="/thread")
+app.register_blueprint(post,url_prefix="/post")
