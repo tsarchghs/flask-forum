19e0e915f14094e06acd2bdb66261b051bc978af
gjergjk71 <gjergjk71@gmail.com>
2018-08-21 10:23:57 +0200

    Defined create_model_instance method

--- application/cbv.py
+++ application/cbv.py
@@ -9,25 +9,32 @@ class CreateView(MethodView):
 		self.form = form
 		self.model = model
 		self.db = db
-		self.url = url
+		self.redirect_url = url
 
 	def get_post_redirect_args(self,model_instance):
 		raise NotImplementedError
 
-	def get(self):
+	def create_model_instance(self,form):
+		raise NotImplementedError
+
+	def get(self,**kwargs):
 		form = self.form(request.form)
 		if current_user.account_type != "administrator":
 			abort(401)
 		return render_template(self.template_name,form=form)
 
-	def post(self):
+	def post(self,**kwargs):
 		form = self.form(request.form)
 		if current_user.account_type != "administrator":
 			abort(401)
 		if not form.validate():
 			return render_template(self.template_name,form=form)
-		model_instance = self.model(form.name.data)
+		model_instance = self.create_model_instance(form)
 		self.db.session.add(model_instance)
 		self.db.session.commit()
 		args = self.get_post_redirect_args(model_instance)
 		return redirect(url_for(self.url,**args))
+
+"""for field in self.model.__table__.columns:
+args[model_instance.field] = form.field.data
+model_instance = model(**args)"""
